28d1be4b4a9d4bc7ce4f06011164437453d1655b
IsuruFerna <isurumadushan.ferna@gmail.com>
2022-09-04 23:33:07 +0200

    2d arrays and nested loops

---
+++ for-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  int luckyNumbers[] = {4, 3, 5, 12, 8, 0};
+  
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%d\n", luckyNumbers[i]);
+  }
+
+
+  return 0;
+}
